907cb75d9f7161fe4ae04c2d7d14d2ef28c596c0
Farzin Houshmand <farzinh@google.com>
2024-12-12 17:44:38 -0800

    Make max value in Range optional to allow for Unbounded Range calculations.
    Also, cache the intermediate calculated ranges when calling RecrusivelyIdentifyRange.
    
    PiperOrigin-RevId: 705687531

--- third_party/xla/xla/service/value_range.h
+++ third_party/xla/xla/service/value_range.h
@@ -35,14 +35,15 @@ class Range {
         step_(ConstantValue::GetZero(/*bitwidth=*/64, /*is_signed=*/false)),
         empty_(true),
         is_linear_(false) {}
-  Range(const ConstantValue& min, const ConstantValue& max, bool is_linear)
+  Range(const ConstantValue& min, std::optional<ConstantValue> max,
+        bool is_linear)
       : min_(min),
         max_(max),
         step_(std::nullopt),
         empty_(false),
         is_linear_(is_linear) {}
-  Range(const ConstantValue& min, const ConstantValue& max,
-        std::optional<ConstantValue> step, bool is_linear)
+  Range(const ConstantValue& min, std::optional<ConstantValue> max,
+        ConstantValue step, bool is_linear)
       : min_(min),
         max_(max),
         step_(step),
@@ -51,7 +52,7 @@ class Range {
   // Minimum value of the range.
   const ConstantValue& min() const { return min_; }
   // Maximum value of the range.
-  const ConstantValue& max() const { return max_; }
+  const std::optional<ConstantValue>& max() const { return max_; }
   // Step value of the range.
   const ConstantValue& step() const { return step_.value(); }
   // Returns if the range is empty (no value in set).
@@ -65,6 +66,8 @@ class Range {
   // loop recursion.
   bool IsLinear() const { return is_linear_; }
   bool IsStepKnown() const { return step_.has_value(); }
+  // If this range is a bounded range with known max value.
+  bool IsBounded() const { return max_.has_value(); }
   // If this range represents a single value return that signed value.
   std::optional<int64_t> GetSingleSignedValue() const;
   // If this range represents a single value return that unsigned value.
@@ -81,20 +84,20 @@ class Range {
 
  private:
   ConstantValue min_;
-  ConstantValue max_;
+  std::optional<ConstantValue> max_;
   std::optional<ConstantValue> step_;
   bool empty_;
   bool is_linear_;
 };
 
-// Constructs a Range object from a HloInstruction. Gets a "predefined_ranges"
+// Constructs a Range object from a HloInstruction. Gets a "known_ranges"
 // object as input that returns known ranges for some variables for which we
 // already know the range. The final range is composed from operations over
 // these predetermined ranges.
 // The input HLO needs to be of scalar type and integer.
 Range RecursivelyIdentifyRange(
     const HloInstruction* instr,
-    const absl::flat_hash_map<const HloInstruction*, Range>& predefined_ranges,
+    absl::flat_hash_map<const HloInstruction*, Range>& known_ranges,
     const HloAliasAnalysis* alias_analysis = nullptr);
 
 }  // namespace xla
